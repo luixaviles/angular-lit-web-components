33a8ec295474dead5284adcc8336051e05bc012c
luixaviles <luis.avilesz@gmail.com>
2021-10-01 16:48:46 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'corp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-lit-web-components';
+}
